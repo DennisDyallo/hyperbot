a6e8708c73cd29886251ab697af933d12983423d
Dennis Dyallo <dennis.dyall@yubico.com>
2025-11-29 02:23:08 +0100

    Add Artifact Registry Writer permission to setup script

--- setup-github-actions.sh
+++ setup-github-actions.sh
@@ -50,6 +50,12 @@ gcloud projects add-iam-policy-binding ${PROJECT_ID} \
     --role="roles/storage.admin" \
     --quiet
 
+echo "  → Artifact Registry Writer"
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+    --member="serviceAccount:${SERVICE_ACCOUNT_EMAIL}" \
+    --role="roles/artifactregistry.writer" \
+    --quiet
+
 echo "  → Service Account User"
 gcloud projects add-iam-policy-binding ${PROJECT_ID} \
     --member="serviceAccount:${SERVICE_ACCOUNT_EMAIL}" \
