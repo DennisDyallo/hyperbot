e7488c7f7a70126cd06c77b7e57a4d048fd5c4e7
Dennis Dyallo <dennis.dyall@yubico.com>
2025-11-29 02:28:04 +0100

    Add Artifact Registry Repo Admin permission for auto-creation

--- setup-github-actions.sh
+++ setup-github-actions.sh
@@ -56,6 +56,12 @@ gcloud projects add-iam-policy-binding ${PROJECT_ID} \
     --role="roles/artifactregistry.writer" \
     --quiet
 
+echo "  → Artifact Registry Repo Admin"
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+    --member="serviceAccount:${SERVICE_ACCOUNT_EMAIL}" \
+    --role="roles/artifactregistry.repoAdmin" \
+    --quiet
+
 echo "  → Service Account User"
 gcloud projects add-iam-policy-binding ${PROJECT_ID} \
     --member="serviceAccount:${SERVICE_ACCOUNT_EMAIL}" \
