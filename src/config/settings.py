809be79d14fb99dc127f34d0554b90fb14216a5f
Dennis Dyallo <dennis.dyall@yubico.com>
2025-12-07 11:22:20 +0100

    chore: update GCP deployment trigger to allow pushes from all branches

--- src/config/settings.py
+++ src/config/settings.py
@@ -50,7 +50,7 @@ class Settings:
 
     # Project
     PROJECT_NAME: str = "Hyperbot"
-    VERSION: str = "0.1.0+ee7c8fb"
+    VERSION: str = "0.1.0+8f1b7f1"
     ENVIRONMENT: str = os.getenv("ENVIRONMENT", "development")
 
     # API Server
